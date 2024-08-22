79c408af09cd1cf3539ac6ebdbe99e9e0a9b97d3
Justin <rayjustin481@gmail.com>
2024-08-22 18:07:56 +0300

    Added a filter feature to fiter foods

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,24 +3,45 @@ import Item from "./Item";
 
 function Search({ foods, addToCart }) {
   const [searchTerm, setSearchTerm] = useState("");
+  const [category, setCategory] = useState("");
 
   const handleInputChange = (e) => {
     setSearchTerm(e.target.value);
   };
 
-  const filteredFoods = foods.filter((food) =>
-    food.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const handleFilterChange = (e) => {
+    setCategory(e.target.value);
+  };
+
+  const filteredFoods = foods.filter((food) => {
+    const matchesCategory = category ? food.category === category : true;
+    const matchesSearchTerm = food.name
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+    return matchesCategory && matchesSearchTerm;
+  });
 
   return (
     <div>
-      <input
-        type="text"
-        className="searchbox p-1 border border-gray-600 mt-4 mb-4 rounded shadow-md"
-        placeholder="Search Meal..."
-        value={searchTerm}
-        onChange={handleInputChange}
-      />
+      <div className="flex flex-col sm:flex-row items-center justify-between gap-4 mb-4">
+        <input
+          type="text"
+          className="searchbox w-full sm:w-1/2 p-2 border border-gray-600 rounded shadow-md"
+          placeholder="Search Meal..."
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+        <select
+          className="w-full sm:w-1/3 p-2 border border-gray-400 rounded shadow-md"
+          value={category}
+          onChange={handleFilterChange}
+        >
+          <option value="">All Categories</option>
+          <option value="Tea">Tea</option>
+          <option value="Dessert">Desserts</option>
+          <option value="Brunch">Brunch</option>
+        </select>
+      </div>
       <div className="flex flex-wrap justify-center">
         {filteredFoods.map((food) => (
           <Item key={food.id} food={food} addToCart={addToCart} />
